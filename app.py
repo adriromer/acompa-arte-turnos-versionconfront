3229f14a7ee8dd174af97950a0be2d4ed7a185f3
adriromer <estoesunacuentademail@gmail.com>
2020-03-18 01:47:25 -0300

    git ignore y borro virtual env

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def logout():
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     print(config['host'],config['port'])
-    app.run(debug=True,host='0.0.0.0',port=8080)
+    app.run(debug=True,host=config['host'],port=config['port'])
